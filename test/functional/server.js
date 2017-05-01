2dfa9a5e72876575cb11c7f97c84be0ef25ebd0d
Ghafran <ghafran@avizia.com>
2017-05-01 15:21:26 -0400

    sdp

--- test/functional/server.js
+++ test/functional/server.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var mcu = require('../../lib/mcu');
+
+var conferences = {};
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/join', function (req, res) {
+
+    var conference = conferences[req.body.conference_id];
+    if(!conference) {
+        conference = new mcu.Conference({
+            conferenceId: req.body.conference_id,
+            defaultView: 'composite'
+        });
+    }
+
+    var participant = new mcu.WebRTCParticipant({
+        participant_id: req.body.participant_id,
+        offer: req.body.offer,
+    });
+
+    participant.join(conference).then((answer) => {
+        console.log('join with answer.', answer);
+        res.json({
+            answer: answer
+        });
+    });
+
 });
 
 app.listen(3000);
--- test/unit/webrtcparticipant.js
+++ test/unit/webrtcparticipant.js
@@ -17,12 +17,10 @@ describe('WebRTCParticipant', () => {
             expect(conference).to.exist;
 
             var offer = fs.readFileSync(__dirname + '/assets/webrtc_offer', 'utf8');
-            var iceCandidatesAll = fs.readFileSync(__dirname + '/assets/webrtc_ice_candidates', 'utf8');
-            var iceCandidates = iceCandidatesAll.split('\n');
             var participant = new mcu.WebRTCParticipant({
                 participant_id: chance.word(),
                 offer: offer,
-                iceCandidates: iceCandidates
+                iceCandidates: []
             });
             expect(participant).to.exist;
             return participant.join(conference).then((answer) => {
